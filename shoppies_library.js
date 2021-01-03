33e7e0a74a3382ea2335b3e3d79d3c7536119803
Stanley <zystanley.wu@mail.utoronto.ca>
2021-01-03 02:47:05 -0500

    got UI list working

--- shoppies_library.js
+++ shoppies_library.js
@@ -4,40 +4,44 @@ const URL = 'https://www.omdbapi.com/?apikey=a23db7da&s='
 const searchForm = document.querySelector('#search form');
 const resultsDiv = document.querySelector('#results');
 const resultTitleDiv = document.querySelector('#results h3');
-const resultTable = document.querySelector('#results table');
+const resultPages = document.querySelector('#results table');
 
+let currentPage = 0;
 let responseList = []; // track response from page=1, 2, ... so on
+let nominatedList = []; // track nominated movies, max = 5
 searchForm.addEventListener('submit', collectResult);
 
 async function collectResult(e) {
 	e.preventDefault();
 	responseList = []; // reset the list of API collection
 
+	// grab everything from API call
 	let movieEntered = searchForm.querySelector('input').value;
 	let omdbResultSize = await getResultSize(movieEntered);
 	let noMoviesFound = (omdbResultSize == 0);
 	let totalPages = Math.ceil(omdbResultSize / 10);
-	// console.log(`Movie: ${movieEntered}, Pages: ${totalPages}`);
+	console.log(`Movie: ${movieEntered}, Pages: ${totalPages}`);
 	let i = 1;
 	while (omdbResultSize > 0) {
 		let batchSize = await callAPI(movieEntered, i);
 		omdbResultSize -= batchSize;
 		i++;
 	}
-	// TODO: output responseList to unordered list
-	let oldList = document.querySelector('#results ul');
-	let newList = document.createElement('ul');
+
+	// what to display if no movie title matches
 	if (noMoviesFound) {
+		let oldList = document.querySelector('#results ul');
+		let newList = document.createElement('ul');
 		let newListEl = document.createElement('li');
 		newListEl.innerHTML = `No movies match "${movieEntered}"!`;
 		newList.appendChild(newListEl);
+		resultsDiv.replaceChild(newList, oldList);
+	} else {
+
 	}
-	else {
-		// TODO: case when results from API != NULL
-	}
-	resultsDiv.replaceChild(newList, oldList);
 
 	// number of totalPages to access search results
+	// within this, it'll also generate the results view
 	displayPageNumber(totalPages);
 
 	// change the heading
@@ -72,26 +76,43 @@ async function getResultSize(movieEntered) {
 }
 
 function displayPageNumber(totalPages) {
-	// replace the old table body with a new and empty one
-	let oldTableBody = document.querySelector('#results table tbody');
-	let newTableBody = document.createElement('tbody');
-	let newTableRow = document.createElement('tr');
-	newTableBody.appendChild(newTableRow);
-	resultTable.replaceChild(newTableBody, oldTableBody);
+	// replace the old page body with a new and empty one
+	let oldPageBody = document.querySelector('#results table tbody');
+	let newPageBody = document.createElement('tbody');
+	let newPageRow = document.createElement('tr');
+	newPageBody.appendChild(newPageRow);
+	resultPages.replaceChild(newPageBody, oldPageBody);
 
 	// append page number to each table cell
 	let j = 0;
 	while (totalPages > 0) {
-		let cell = newTableRow.insertCell(j);
+		let cell = newPageRow.insertCell(j);
 		cell.style.minWidth = '30px'; // can take out after CSS is done
 		cell.style.textAlign = 'center';
 		cell.innerHTML = j + 1;
+		cell.addEventListener('click', generateResultsView);
 		totalPages -= 1;
 		j++;
 	}
+	newPageRow.firstChild.click();
+
 }
 
-
+function generateResultsView(e) {
+	console.log(`Clicked on ${this.innerHTML}`);
+	// create the UI for all the movie results in this page number
+	let currentPageNum = parseInt(this.innerHTML);
+	let resultsOnCurrentPage = responseList[currentPageNum - 1];
+	let oldList = document.querySelector('#results ul');
+	let newList = document.createElement('ul');
+	for(let i = 0; i < resultsOnCurrentPage.length; i++) {
+		let newListEl = document.createElement('li');
+		let ithResult = resultsOnCurrentPage[i]
+		newListEl.innerHTML = `${ithResult["Title"]} (${ithResult["Year"]}) Type: ${ithResult["Type"]}`;
+		newList.appendChild(newListEl);
+	}
+	resultsDiv.replaceChild(newList, oldList);
+}
 
 
 
