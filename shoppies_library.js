64422f93cedea8386d340e8e8db944f6d65d9924
Stanley <zystanley.wu@mail.utoronto.ca>
2021-01-04 02:28:57 -0500

    finished UI part, some nominations part

--- shoppies_library.js
+++ shoppies_library.js
@@ -1,51 +1,51 @@
 /* Library - JS */
 "use strict";
-const URL = 'https://www.omdbapi.com/?apikey=a23db7da&s='
-const searchForm = document.querySelector('#search form');
-const resultsDiv = document.querySelector('#results');
-const resultTitleDiv = document.querySelector('#results h3');
-const resultPages = document.querySelector('#results table');
-
-let currentPage = 0;
+const URL = 'https://www.omdbapi.com/?apikey=a23db7da&s=';
+const NO_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
+const ALERT = 'https://commons.wikimedia.org/wiki/File:OOjs_UI_icon_alert-yellow.svg';
+const NOMINATION_LIMIT = 5;
 let responseList = []; // track response from page=1, 2, ... so on
-let nominatedList = []; // track nominated movies, max = 5
-searchForm.addEventListener('submit', collectResult);
+let nominatedMovies = new Set(); // track nominated movies, max = 5
 
-async function collectResult(e) {
-	e.preventDefault();
+$(function(){
+	// generate API results UI when user initiates search
+	$("#search form").on('submit', function(event) {
+		event.preventDefault();
+		collectResult();
+	});
+	// TODO: generate nominate section
+});
+
+
+const searchForm = document.querySelector('#search form');
+const resultsDiv = document.querySelector('#resultsDiv');
+const resultTitleDiv = document.querySelector('#resultsDiv h2');
+const resultPages = document.querySelector('#resultsDiv table');
+
+async function collectResult() {
 	responseList = []; // reset the list of API collection
-
-	// grab everything from API call
 	let movieEntered = searchForm.querySelector('input').value;
 	let omdbResultSize = await getResultSize(movieEntered);
 	let noMoviesFound = (omdbResultSize == 0);
 	let totalPages = Math.ceil(omdbResultSize / 10);
-	console.log(`Movie: ${movieEntered}, Pages: ${totalPages}`);
+	// console.log(`Movie: ${movieEntered}, Pages: ${totalPages}`);
 	let i = 1;
 	while (omdbResultSize > 0) {
 		let batchSize = await callAPI(movieEntered, i);
 		omdbResultSize -= batchSize;
 		i++;
 	}
-
 	// what to display if no movie title matches
 	if (noMoviesFound) {
-		let oldList = document.querySelector('#results ul');
-		let newList = document.createElement('ul');
-		let newListEl = document.createElement('li');
-		newListEl.innerHTML = `No movies match "${movieEntered}"!`;
-		newList.appendChild(newListEl);
-		resultsDiv.replaceChild(newList, oldList);
+		$('.listUI').empty();
+		$('#resultsDiv tr').empty();
+		$('.listUI').append(`<p>No movies match "${movieEntered}"!</p>`);
 	} else {
-
+		displayPageNumber(totalPages);
 	}
 
-	// number of totalPages to access search results
-	// within this, it'll also generate the results view
-	displayPageNumber(totalPages);
-
 	// change the heading
-	resultTitleDiv.innerHTML = `Results for "${movieEntered}"`
+	$('#resultsDiv h2').html(`Results for "${movieEntered}"`);
 }
 
 async function callAPI(movieEntered, i) {
@@ -77,70 +77,72 @@ async function getResultSize(movieEntered) {
 
 function displayPageNumber(totalPages) {
 	// replace the old page body with a new and empty one
-	let oldPageBody = document.querySelector('#results table tbody');
-	let newPageBody = document.createElement('tbody');
-	let newPageRow = document.createElement('tr');
-	newPageBody.appendChild(newPageRow);
-	resultPages.replaceChild(newPageBody, oldPageBody);
-
+	$('#resultsDiv table').empty();
+	let $pageNums = $('<tr>');
+	$('#resultsDiv table').append($pageNums);
 	// append page number to each table cell
 	let j = 0;
 	while (totalPages > 0) {
-		let cell = newPageRow.insertCell(j);
-		cell.style.minWidth = '30px'; // can take out after CSS is done
-		cell.style.textAlign = 'center';
-		cell.innerHTML = j + 1;
-		cell.addEventListener('click', generateResultsView);
+		let $cell = $('<td>', {'style': 'min-width: 30px; text-align: center;'});
+		$cell.html(`${j + 1}`);
+		console.log(`${$cell.html()}`)
+		$cell.click(generateResultsView);
+		$pageNums.append($cell);
 		totalPages -= 1;
 		j++;
 	}
-	newPageRow.firstChild.click();
-
+	if ($('#resultsDiv td').first() != null){
+		$('#resultsDiv td').first().click();
+	}
 }
 
 function generateResultsView(e) {
-	console.log(`Clicked on ${this.innerHTML}`);
+	console.log(`Clicked on page ${this.innerHTML}`);
 	// create the UI for all the movie results in this page number
-	let currentPageNum = parseInt(this.innerHTML);
-	let resultsOnCurrentPage = responseList[currentPageNum - 1];
-	let oldList = document.querySelector('#results ul');
-	let newList = document.createElement('ul');
+	let resultsOnCurrentPage = responseList[this.innerHTML - 1];
+	$('#resultsDiv .listUI').empty();
 	for(let i = 0; i < resultsOnCurrentPage.length; i++) {
-		let newListEl = document.createElement('li');
-		let ithResult = resultsOnCurrentPage[i]
-		newListEl.innerHTML = `${ithResult["Title"]} (${ithResult["Year"]}) Type: ${ithResult["Type"]}`;
-		newList.appendChild(newListEl);
+		let ithResult = resultsOnCurrentPage[i];
+		let $cell = $('<div>', {'class': 'singleMovie', 'imdbID': `${ithResult["imdbID"]}`});
+		$cell.append(`<img src=${ithResult["Poster"]} onerror=this.src='${NO_IMAGE}'; width="200">`);
+		$cell.append(`<h4>${ithResult["Title"]} (${ithResult["Year"]})</h4>`);
+		$cell.append(`<p>Type: ${ithResult["Type"]}</p>`);
+		$cell.append(createNominateButton(ithResult["imdbID"]));
+		$cell.append(`<br></br>`);
+		$('#resultsDiv .listUI').append($cell);
 	}
-	resultsDiv.replaceChild(newList, oldList);
 }
 
-
-
-
-
-// Book 'class'
-class Book {
-	constructor(title, author, genre) {
-		this.title = title;
-		this.author = author;
-		this.genre = genre;
-		this.patron = null; // will be the patron objet
-
-		// set book ID
-		this.bookId = numberOfBooks;
-		numberOfBooks++;
+function createNominateButton(imdbID){
+	let $nominateButton = $('<button>', {'type': 'button'});
+	if (nominatedMovies.has(imdbID)) {
+		$nominateButton.html('Remove');
+	} else {
+		$nominateButton.html('Nominate');
 	}
+	$nominateButton.click(function(e) {
+		processNominate(imdbID, this);
+	});
+	return $nominateButton;
+}
 
-	setLoanTime() {
-		// Create a setTimeout that waits 3 seconds before indicating a book is overdue
-
-		const self = this; // keep book in scope of anon function (why? the call-site for 'this' in the anon function is the DOM window)
-		setTimeout(function() {
-			
-			console.log('overdue book!', self.title)
-			changeToOverdue(self);
-
-		}, 3000)
-
+function processNominate(imdbID, button) {
+	if (nominatedMovies.has(imdbID)) {
+		nominatedMovies.delete(imdbID);
+		button.innerHTML = 'Nominate';
+	}
+	else {
+		if (nominatedMovies.size < NOMINATION_LIMIT) {
+			nominatedMovies.add(imdbID);
+			// TODO: create the UI in under the nominated header UI
+			button.innerHTML = 'Remove';
+		} else {
+			// alert user to remove a nomination
+			// TODO: google "javascript popup with timer"
+			// https://www.w3schools.com/howto/howto_js_alert.asp
+			let limitAlert = document.createElement('p');
+			limitAlert.innerHTML = `Max of ${NOMINATION_LIMIT} nominations!\nRemove some!`;
+			button.after(limitAlert);
+		}
 	}
 }
